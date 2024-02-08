bfb968a09792fb185d49523747b5ee2107207853
Shrikant <shrikant.awhad.58@gmail.com>
2024-02-08 14:44:44 +0530

    Journal App First Commit

---
+++ src/main/java/net/engineeringdigest/journalApp/JournalAppApplication.java
@@ -0,0 +1,13 @@
+package net.engineeringdigest.journalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalAppApplication.class, args);
+	}
+
+}
