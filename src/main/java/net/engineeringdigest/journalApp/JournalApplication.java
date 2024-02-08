7f0a7a5d3d23e4b39d88322e2b351fe176e90c1f
Shrikant <shrikant.awhad.58@gmail.com>
2024-02-08 15:18:02 +0530

    Added controller package

--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JournalAppApplication {
+public class JournalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JournalAppApplication.class, args);
+		SpringApplication.run(JournalApplication.class, args);
 	}
 
 }
---
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,4 @@
+package net.engineeringdigest.journalApp.controller;
+
+public class JournalEntryController {
+}
